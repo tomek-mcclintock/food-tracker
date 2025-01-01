63a507404ce74df1432b9d2729b1c0963f780217
tomek-mcclintock <tomekmcc@gmail.com>
2025-01-01 17:45:57 +0100

    Fix

--- src/middleware.js
+++ src/middleware.js
@@ -25,5 +25,5 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|manifest.json|sw.js|icons).*)'],
 };
