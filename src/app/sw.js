5fdeae309212a102657f477c97a21384266cce8e
tomek-mcclintock <tomekmcc@gmail.com>
2025-01-01 19:06:58 +0100

    disable sw

--- src/app/sw.js
+++ src/app/sw.js
@@ -1,5 +1,7 @@
 "use client";
 
+// Temporarily disabled service worker
+/* 
 if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker
@@ -12,3 +14,4 @@ if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
       });
   });
 }
+*/
